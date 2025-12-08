f6385bd7f612e141583917652d4139c29acb82cd
Lukrecja <lusiaaa425@gmail.com>
2025-12-08 17:01:36 +0100

    Update admin credentials and add MLP-themed test users

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -12,12 +12,33 @@ class AdminUserSeeder extends Seeder
     {
         User::create([
             'name' => 'Admin User',
-            'email' => 'admin@quiz.app',
-            'password' => Hash::make('password'),
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin'),
             'is_admin' => true,
             'email_verified_at' => now(),
         ]);
 
-        User::factory()->count(10)->create();
+        $mlpPonies = [
+            'Twilight Sparkle',
+            'Starlight Glimmer',
+            'Fluttershy',
+            'Rainbow Dash',
+            'Pinkie Pie',
+            'Rarity',
+            'Applejack',
+            'Sunset Shimmer',
+            'Princess Celestia',
+            'Princess Luna',
+        ];
+
+        foreach ($mlpPonies as $index => $name) {
+            User::create([
+                'name' => $name,
+                'email' => 'user' . ($index + 1) . '@example.com',
+                'password' => Hash::make('1234'),
+                'is_admin' => false,
+                'email_verified_at' => now(),
+            ]);
+        }
     }
 }
